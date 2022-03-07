ee00409dc92567cb8f91d955c6a0956e275fe819
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-07 01:08:03 -0500

    add weather APIKey and select inputs form

---
+++ weatherApp/script.js
@@ -0,0 +1,4 @@
+const API_key = 'cab8e12454cc911bd2fc51714a0dafa1'
+const cityInput = document.querySelector(".city")
+const stateInput = document.querySelector(".state")
+const weatherForm = document.querySelector("#weatherForm")
