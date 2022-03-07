6e175f3668e24b372c5dc4e4fc13b889367312b1
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-07 01:21:50 -0500

    initialize API and interpolate input to API

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -1,4 +1,8 @@
 const API_key = 'cab8e12454cc911bd2fc51714a0dafa1'
 const cityInput = document.querySelector(".city")
 const stateInput = document.querySelector(".state")
+const countryCode = "us"
 const weatherForm = document.querySelector("#weatherForm")
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput},${stateInput},${countryCode}&appid=${API_key}&units=imperial`)
+.then((resp)=> resp.json())
