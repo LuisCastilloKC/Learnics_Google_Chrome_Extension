9282cfbc2855cd91c531eb008dbd1f408cc6fdd7
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-07 04:00:21 -0500

    offHandler

--- alertTimer/scriptAlert.js
+++ alertTimer/scriptAlert.js
@@ -3,5 +3,11 @@ let alarmClock = {
     onHandler : function() {
         chrome.alarms.create("myAlarm", {delayInMinutes: 0.1, periodInMinutes: 0.2} );
                 window.close();
+    },
+
+    offHandler : function() {
+        chrome.alarms.clear("myAlarm");
+                window.close();
     }
+
 }
